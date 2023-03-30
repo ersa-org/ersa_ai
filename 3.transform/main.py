6e5c93f4f17c101705236e7eaecc92de8d41fb08
杨国平 <756517553@qq.com>
2023-03-31 03:55:45 +0800

    feat:fix

--- 3.transform/main.py
+++ 3.transform/main.py
@@ -8,15 +8,23 @@ from torch.utils.tensorboard import SummaryWriter
 # 为什么要用tensor类型
 
 
-
 if __name__ == '__main__':
     img_path = "/Users/panzi/Desktop/git_pro/ersa_ai/1.dataset/F8DDCD9E-8CED-4ebe-BB5D-F884747880B5.png"
     img= Image.open(img_path)
     tensor = transforms.ToTensor()
     tensor_img = tensor(img)
-    print(tensor_img)
+    # print(tensor_img)
     writer = SummaryWriter("logs")
     writer.add_image("tensor image",tensor_img)
+
+    t_n=transforms.Normalize([2,3,4],[3,4,3])
+    normal_img =t_n(tensor_img)
+    print("normal_image")
+    print(normal_img)
+
+
+
+
     writer.close()
 
 
